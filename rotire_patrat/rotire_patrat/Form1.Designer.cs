1ae500c829063908100c363337bf7c64721c73db
Chirodea Moise <tiii3139@gmail.com>
2021-03-10 20:51:28 +0200

    rotire_patrat

---
+++ rotire_patrat/rotire_patrat/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace PatratRotit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pB = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.xa = new System.Windows.Forms.TextBox();
+            this.ya = new System.Windows.Forms.TextBox();
+            this.xb = new System.Windows.Forms.TextBox();
+            this.yb = new System.Windows.Forms.TextBox();
+            this.xc = new System.Windows.Forms.TextBox();
+            this.yc = new System.Windows.Forms.TextBox();
+            this.xd = new System.Windows.Forms.TextBox();
+            this.yd = new System.Windows.Forms.TextBox();
+            this.Rotire = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pB
+            // 
+            this.pB.Location = new System.Drawing.Point(8, 8);
+            this.pB.Margin = new System.Windows.Forms.Padding(2);
+            this.pB.Name = "pB";
+            this.pB.Size = new System.Drawing.Size(597, 407);
+            this.pB.TabIndex = 0;
+            this.pB.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(621, 298);
+            this.Start.Margin = new System.Windows.Forms.Padding(2);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(109, 83);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // xa
+            // 
+            this.xa.Location = new System.Drawing.Point(647, 30);
+            this.xa.Margin = new System.Windows.Forms.Padding(2);
+            this.xa.Name = "xa";
+            this.xa.Size = new System.Drawing.Size(32, 20);
+            this.xa.TabIndex = 2;
+            this.xa.TextChanged += new System.EventHandler(this.xa_TextChanged);
+            // 
+            // ya
+            // 
+            this.ya.Location = new System.Drawing.Point(683, 30);
+            this.ya.Margin = new System.Windows.Forms.Padding(2);
+            this.ya.Name = "ya";
+            this.ya.Size = new System.Drawing.Size(32, 20);
+            this.ya.TabIndex = 3;
+            // 
+            // xb
+            // 
+            this.xb.Location = new System.Drawing.Point(803, 30);
+            this.xb.Margin = new System.Windows.Forms.Padding(2);
+            this.xb.Name = "xb";
+            this.xb.Size = new System.Drawing.Size(32, 20);
+            this.xb.TabIndex = 4;
+            // 
+            // yb
+            // 
+            this.yb.Location = new System.Drawing.Point(839, 30);
+            this.yb.Margin = new System.Windows.Forms.Padding(2);
+            this.yb.Name = "yb";
+            this.yb.Size = new System.Drawing.Size(32, 20);
+            this.yb.TabIndex = 5;
+            // 
+            // xc
+            // 
+            this.xc.Location = new System.Drawing.Point(647, 82);
+            this.xc.Margin = new System.Windows.Forms.Padding(2);
+            this.xc.Name = "xc";
+            this.xc.Size = new System.Drawing.Size(32, 20);
+            this.xc.TabIndex = 6;
+            // 
+            // yc
+            // 
+            this.yc.Location = new System.Drawing.Point(683, 82);
+            this.yc.Margin = new System.Windows.Forms.Padding(2);
+            this.yc.Name = "yc";
+            this.yc.Size = new System.Drawing.Size(32, 20);
+            this.yc.TabIndex = 7;
+            // 
+            // xd
+            // 
+            this.xd.Location = new System.Drawing.Point(803, 82);
+            this.xd.Margin = new System.Windows.Forms.Padding(2);
+            this.xd.Name = "xd";
+            this.xd.Size = new System.Drawing.Size(32, 20);
+            this.xd.TabIndex = 8;
+            // 
+            // yd
+            // 
+            this.yd.Location = new System.Drawing.Point(839, 82);
+            this.yd.Margin = new System.Windows.Forms.Padding(2);
+            this.yd.Name = "yd";
+            this.yd.Size = new System.Drawing.Size(32, 20);
+            this.yd.TabIndex = 9;
+            // 
+            // Rotire
+            // 
+            this.Rotire.Location = new System.Drawing.Point(754, 298);
+            this.Rotire.Margin = new System.Windows.Forms.Padding(2);
+            this.Rotire.Name = "Rotire";
+            this.Rotire.Size = new System.Drawing.Size(112, 83);
+            this.Rotire.TabIndex = 10;
+            this.Rotire.Text = "Rotire";
+            this.Rotire.UseVisualStyleBackColor = true;
+            this.Rotire.Click += new System.EventHandler(this.Rotire_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(605, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "xA & yA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(605, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "xC & yC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(761, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "xB & yB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(759, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "xD & yD";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 419);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Rotire);
+            this.Controls.Add(this.yd);
+            this.Controls.Add(this.xd);
+            this.Controls.Add(this.yc);
+            this.Controls.Add(this.xc);
+            this.Controls.Add(this.yb);
+            this.Controls.Add(this.xb);
+            this.Controls.Add(this.ya);
+            this.Controls.Add(this.xa);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pB);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox xa;
+        private System.Windows.Forms.TextBox ya;
+        private System.Windows.Forms.TextBox xb;
+        private System.Windows.Forms.TextBox yb;
+        private System.Windows.Forms.TextBox xc;
+        private System.Windows.Forms.TextBox yc;
+        private System.Windows.Forms.TextBox xd;
+        private System.Windows.Forms.TextBox yd;
+        private System.Windows.Forms.Button Rotire;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
