b53e1ad7089316982d31110a88ec926fd8f82fe0
Chirodea Moise <tiii3139@gmail.com>
2021-03-10 20:24:10 +0200

    Cuse3_linii importante in triunghi

---
+++ Curs3_V2/Curs3_V2/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Linii_triunghi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pB = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.yc = new System.Windows.Forms.TextBox();
+            this.xc = new System.Windows.Forms.TextBox();
+            this.yb = new System.Windows.Forms.TextBox();
+            this.xb = new System.Windows.Forms.TextBox();
+            this.ya = new System.Windows.Forms.TextBox();
+            this.xa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pB
+            // 
+            this.pB.Location = new System.Drawing.Point(8, 1);
+            this.pB.Margin = new System.Windows.Forms.Padding(2);
+            this.pB.Name = "pB";
+            this.pB.Size = new System.Drawing.Size(550, 360);
+            this.pB.TabIndex = 0;
+            this.pB.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(589, 31);
+            this.Start.Margin = new System.Windows.Forms.Padding(2);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(74, 41);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(589, 114);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Mediana";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(589, 159);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Mediatoare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(589, 200);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 27);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Inaltime";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(589, 239);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Bisectoare";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // yc
+            // 
+            this.yc.Location = new System.Drawing.Point(791, 107);
+            this.yc.Margin = new System.Windows.Forms.Padding(2);
+            this.yc.Name = "yc";
+            this.yc.Size = new System.Drawing.Size(32, 20);
+            this.yc.TabIndex = 13;
+            // 
+            // xc
+            // 
+            this.xc.Location = new System.Drawing.Point(755, 107);
+            this.xc.Margin = new System.Windows.Forms.Padding(2);
+            this.xc.Name = "xc";
+            this.xc.Size = new System.Drawing.Size(32, 20);
+            this.xc.TabIndex = 12;
+            // 
+            // yb
+            // 
+            this.yb.Location = new System.Drawing.Point(791, 68);
+            this.yb.Margin = new System.Windows.Forms.Padding(2);
+            this.yb.Name = "yb";
+            this.yb.Size = new System.Drawing.Size(32, 20);
+            this.yb.TabIndex = 11;
+            // 
+            // xb
+            // 
+            this.xb.Location = new System.Drawing.Point(755, 68);
+            this.xb.Margin = new System.Windows.Forms.Padding(2);
+            this.xb.Name = "xb";
+            this.xb.Size = new System.Drawing.Size(32, 20);
+            this.xb.TabIndex = 10;
+            // 
+            // ya
+            // 
+            this.ya.Location = new System.Drawing.Point(791, 31);
+            this.ya.Margin = new System.Windows.Forms.Padding(2);
+            this.ya.Name = "ya";
+            this.ya.Size = new System.Drawing.Size(32, 20);
+            this.ya.TabIndex = 9;
+            // 
+            // xa
+            // 
+            this.xa.Location = new System.Drawing.Point(755, 31);
+            this.xa.Margin = new System.Windows.Forms.Padding(2);
+            this.xa.Name = "xa";
+            this.xa.Size = new System.Drawing.Size(32, 20);
+            this.xa.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(704, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "xA & yA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(704, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "xB & yB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(704, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "xC & yC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 488);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yc);
+            this.Controls.Add(this.xc);
+            this.Controls.Add(this.yb);
+            this.Controls.Add(this.xb);
+            this.Controls.Add(this.ya);
+            this.Controls.Add(this.xa);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pB);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox yc;
+        private System.Windows.Forms.TextBox xc;
+        private System.Windows.Forms.TextBox yb;
+        private System.Windows.Forms.TextBox xb;
+        private System.Windows.Forms.TextBox ya;
+        private System.Windows.Forms.TextBox xa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Curs3_V2/Curs3_V2/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linii_triunghi
+{
+    public partial class Form1 : Form
+    {
+        PointF[] t = new PointF[3];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Graphics grp;
+            Bitmap bmp;
+            bmp = new Bitmap(pB.Width, pB.Height);
+            grp = Graphics.FromImage(bmp);
+            PointF[] t = new PointF[3];
+            Pen p = new Pen(Color.Black, 5);
+
+            t[0].X = float.Parse(xa.Text);
+            t[0].Y = float.Parse(ya.Text);
+            t[1].X = float.Parse(xb.Text);
+            t[1].Y = float.Parse(yb.Text);
+            t[2].X = float.Parse(xc.Text);
+            t[2].Y = float.Parse(yc.Text);
+
+
+            float mab = (t[1].Y - t[0].Y) / (t[1].X - t[0].X);
+            float mac = (t[2].Y - t[0].Y) / (t[2].X - t[0].X);
+            float mbc = (t[2].Y - t[1].Y) / (t[2].X - t[1].X);
+            float xmab = (t[0].X + t[1].X) / 2;
+            float ymab = (t[0].Y + t[1].Y) / 2;
+            float xmac = (t[0].X + t[2].X) / 2;
+            float ymac = (t[0].Y + t[2].Y) / 2;
+            float xmbc = (t[2].X + t[1].X) / 2;
+            float ymbc = (t[2].Y + t[1].Y) / 2;
+            Pen m = new Pen(Color.Red, 5);
+            Pen md = new Pen(Color.Green, 5);
+            Pen b = new Pen(Color.Yellow, 5);
+            Pen i = new Pen(Color.Orange, 5);
+            grp.DrawPolygon(p, t);
+
+            pB.Image = bmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics grp;
+            Bitmap bmp;
+            bmp = new Bitmap(pB.Width, pB.Height);
+            grp = Graphics.FromImage(bmp);
+            Pen p = new Pen(Color.Black, 5);
+            PointF[] t = new PointF[3];
+            t[0].X = float.Parse(xa.Text);
+            t[0].Y = float.Parse(ya.Text);
+            t[1].X = float.Parse(xb.Text);
+            t[1].Y = float.Parse(yb.Text);
+            t[2].X = float.Parse(xc.Text);
+            t[2].Y = float.Parse(yc.Text);
+
+
+            float mab = (t[1].Y - t[0].Y) / (t[1].X - t[0].X);
+            float mac = (t[2].Y - t[0].Y) / (t[2].X - t[0].X);
+            float mbc = (t[2].Y - t[1].Y) / (t[2].X - t[1].X);
+            float xmab = (t[0].X + t[1].X) / 2;
+            float ymab = (t[0].Y + t[1].Y) / 2;
+            float xmac = (t[0].X + t[2].X) / 2;
+            float ymac = (t[0].Y + t[2].Y) / 2;
+            float xmbc = (t[2].X + t[1].X) / 2;
+            float ymbc = (t[2].Y + t[1].Y) / 2;
+            Pen m = new Pen(Color.Red, 5);
+            Pen md = new Pen(Color.Green, 5);
+            Pen b = new Pen(Color.Yellow, 5);
+            Pen i = new Pen(Color.Orange, 5);
+            grp.DrawPolygon(p, t);
+            grp.DrawLine(m, t[0].X, t[0].Y, (t[1].X + t[2].X) / 2, (t[1].Y + t[2].Y) / 2);
+            grp.DrawLine(m, t[1].X, t[1].Y, (t[0].X + t[2].X) / 2, (t[0].Y + t[2].Y) / 2);
+            grp.DrawLine(m, t[2].X, t[2].Y, (t[1].X + t[0].X) / 2, (t[1].Y + t[0].Y) / 2);
+            grp.DrawEllipse(p, (t[0].X + t[1].X + t[2].X) / 3, (t[0].Y + t[1].Y + t[2].Y) / 3, 5, 5);
+            grp.DrawPolygon(p, t);
+            pB.Image = bmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics grp;
+            Bitmap bmp;
+            bmp = new Bitmap(pB.Width, pB.Height);
+            grp = Graphics.FromImage(bmp); Random r = new Random();
+            Pen p = new Pen(Color.Black, 5);
+            PointF[] t = new PointF[3];
+            t[0].X = float.Parse(xa.Text);
+            t[0].Y = float.Parse(ya.Text);
+            t[1].X = float.Parse(xb.Text);
+            t[1].Y = float.Parse(yb.Text);
+            t[2].X = float.Parse(xc.Text);
+            t[2].Y = float.Parse(yc.Text);
+
+            float ab = (float)Math.Sqrt((t[1].X - t[0].X) * (t[1].X - t[0].X) + (t[1].Y - t[0].Y) * (t[1].Y - t[0].Y));
+            float ac = (float)Math.Sqrt((t[2].X - t[0].X) * (t[2].X - t[0].X) + (t[2].Y - t[0].Y) * (t[2].Y - t[0].Y));
+            float bc = (float)Math.Sqrt((t[1].X - t[2].X) * (t[1].X - t[2].X) + (t[1].Y - t[2].Y) * (t[1].Y - t[2].Y));
+            float mab = (t[1].Y - t[0].Y) / (t[1].X - t[0].X);
+            float mac = (t[2].Y - t[0].Y) / (t[2].X - t[0].X);
+            float mbc = (t[2].Y - t[1].Y) / (t[2].X - t[1].X);
+            float xmab = (t[0].X + t[1].X) / 2;
+            float ymab = (t[0].Y + t[1].Y) / 2;
+            float xmac = (t[0].X + t[2].X) / 2;
+            float ymac = (t[0].Y + t[2].Y) / 2;
+            float xmbc = (t[2].X + t[1].X) / 2;
+            float ymbc = (t[2].Y + t[1].Y) / 2;
+            Pen m = new Pen(Color.Red, 5);
+            Pen md = new Pen(Color.Green, 5);
+            Pen b = new Pen(Color.Yellow, 5);
+            Pen i = new Pen(Color.Orange, 5);
+            grp.DrawPolygon(p, t);
+            grp.DrawLine(md, (bc * t[0].X + ac * t[1].X + ab * t[2].X) / (ab + ac + bc), (bc * t[0].Y + ac * t[1].Y + ab * t[2].Y) / (ab + ac + bc), xmab, ymab);
+            grp.DrawLine(md, (bc * t[0].X + ac * t[1].X + ab * t[2].X) / (ab + ac + bc), (bc * t[0].Y + ac * t[1].Y + ab * t[2].Y) / (ab + ac + bc), xmac, ymac);
+            grp.DrawLine(md, (bc * t[0].X + ac * t[1].X + ab * t[2].X) / (ab + ac + bc), (bc * t[0].Y + ac * t[1].Y + ab * t[2].Y) / (ab + ac + bc), xmbc, ymbc);
+            grp.DrawEllipse(p, (bc * t[0].X + ac * t[1].X + ab * t[2].X) / (ab + ac + bc), (bc * t[0].Y + ac * t[1].Y + ab * t[2].Y) / (ab + ac + bc), 5, 5);
+            grp.DrawPolygon(p, t);
+            pB.Image = bmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pB.Update();
+            Graphics grp;
+            Bitmap bmp;
+            bmp = new Bitmap(pB.Width, pB.Height);
+            grp = Graphics.FromImage(bmp);
+            Pen p = new Pen(Color.Black, 5);
+            PointF[] t = new PointF[3];
+            t[0].X = float.Parse(xa.Text);
+            t[0].Y = float.Parse(ya.Text);
+            t[1].X = float.Parse(xb.Text);
+            t[1].Y = float.Parse(yb.Text);
+            t[2].X = float.Parse(xc.Text);
+            t[2].Y = float.Parse(yc.Text);
+            float mab = (t[1].Y - t[0].Y) / (t[1].X - t[0].X);
+            float mac = (t[2].Y - t[0].Y) / (t[2].X - t[0].X);
+            float mbc = (t[2].Y - t[1].Y) / (t[2].X - t[1].X);
+            float xmab = (t[0].X + t[1].X) / 2;
+            float ymab = (t[0].Y + t[1].Y) / 2;
+            float xmac = (t[0].X + t[2].X) / 2;
+            float ymac = (t[0].Y + t[2].Y) / 2;
+            float xmbc = (t[2].X + t[1].X) / 2;
+            float ymbc = (t[2].Y + t[1].Y) / 2;
+            Pen m = new Pen(Color.Red, 5);
+            Pen md = new Pen(Color.Green, 5);
+            Pen b = new Pen(Color.Yellow, 5);
+            Pen i = new Pen(Color.Orange, 5);
+            grp.DrawPolygon(p, t);
+            grp.DrawLine(i, t[0].X, t[0].Y, (mbc * t[1].X - t[1].Y + t[0].Y + (1 / mbc) * t[0].X) / (mbc + (1 / mbc)), mbc * ((mbc * t[1].X - t[1].Y + t[0].Y + (1 / mbc) * t[0].X) / (mbc + (1 / mbc)) - t[1].X) + t[1].Y);
+            grp.DrawLine(i, t[1].X, t[1].Y, (mac * t[2].X - t[2].Y + t[1].Y + (1 / mac) * t[1].X) / (mac + (1 / mac)), mac * ((mac * t[2].X - t[2].Y + t[1].Y + (1 / mac) * t[1].X) / (mac + (1 / mac)) - t[2].X) + t[2].Y);
+            grp.DrawLine(i, t[2].X, t[2].Y, (mab * t[0].X - t[0].Y + t[2].Y + (1 / mab) * t[2].X) / (mab + (1 / mab)), mab * ((mab * t[0].X - t[0].Y + t[2].Y + (1 / mab) * t[2].X) / (mab + (1 / mab)) - t[0].X) + t[0].Y);
+            grp.DrawPolygon(p, t);
+
+            pB.Image = bmp;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pB.Update();
+            Graphics grp;
+            Bitmap bmp;
+            bmp = new Bitmap(pB.Width, pB.Height);
+            grp = Graphics.FromImage(bmp); Random r = new Random();
+            Pen p = new Pen(Color.Black, 5);
+            PointF[] t = new PointF[3];
+            t[0].X = float.Parse(xa.Text);
+            t[0].Y = float.Parse(ya.Text);
+            t[1].X = float.Parse(xb.Text);
+            t[1].Y = float.Parse(yb.Text);
+            t[2].X = float.Parse(xc.Text);
+            t[2].Y = float.Parse(yc.Text);
+            float mab = (t[1].Y - t[0].Y) / (t[1].X - t[0].X);
+            float mac = (t[2].Y - t[0].Y) / (t[2].X - t[0].X);
+            float mbc = (t[2].Y - t[1].Y) / (t[2].X - t[1].X);
+            float xmab = (t[0].X + t[1].X) / 2;
+            float ymab = (t[0].Y + t[1].Y) / 2;
+            float xmac = (t[0].X + t[2].X) / 2;
+            float ymac = (t[0].Y + t[2].Y) / 2;
+            float xmbc = (t[2].X + t[1].X) / 2;
+            float ymbc = (t[2].Y + t[1].Y) / 2;
+            Pen m = new Pen(Color.Red, 5);
+            Pen md = new Pen(Color.Green, 5);
+            Pen b = new Pen(Color.Purple, 5);
+            Pen i = new Pen(Color.Orange, 5);
+            grp.DrawPolygon(p, t);
+            grp.DrawLine(b, t[0].X, t[0].Y, xmbc + 10, ymbc + 10);
+            grp.DrawLine(b, t[1].X, t[1].Y, xmac + 10, ymac + 10);
+            grp.DrawLine(b, t[2].X, t[2].Y, xmab + 10, ymab + 10);
+            grp.DrawPolygon(p, t);
+            pB.Image = bmp;
+        }
+    }
+}
